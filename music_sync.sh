58ab0b8f0b3303ec1877ab04866a5bb9a83c229f
Quang Ly <quangdly@gmail.com>
2022-03-22 16:11:00 -0400

    clean up

--- music_sync.sh
+++ music_sync.sh
@@ -20,8 +20,7 @@ DAYS_OLD=1
 echo "********** Start **********" >> "$LOG"
 echo "Running at $TS" >> "$LOG"
 /usr/bin/rsync -rviu --exclude '.DS_Store' --exclude 'Audio Music Apps/' --exclude 'Logic/' --exclude 'source' "${SOURCE_PATH}" "${TARGET_PATH}" >> "${LOG}" 2>&1 --delete --prune-empty-dirs
-# /usr/bin/rsync -ravip  --exclude '.DS_Store' --exclude 'Audio Music Apps/' --exclude 'Logic/' --exclude 'source' "${SOURCE_PATH}" "${TARGET_PATH}" >> "${LOG}" 2>&1 --delete
-# /usr/bin/rsync -ravi --exclude 'dir1/' /Users/quangly/Music/source/ /Volumes/music/destination >> "${LOG}" 2>&1 --delete
+# /usr/bin/rsync -ravi --exclude 'dir1/' /Users/quangly/Music/source/ /Volumes/music/destination >> "${LOG}" 2>&1 --delete --prune-empty-dirs
 echo "Ending at $TS" >> "$LOG"
 echo "Find and remove log files older than ${DAYS_OLD} days" >> "$LOG"
 find /Users/quangly/logs/ -ctime +${DAYS_OLD}d -print0 | xargs -0 rm
